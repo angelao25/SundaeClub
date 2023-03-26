5d6de373791d1f0f563b3d8c764303902a1f0e9f
Angel Aguirre Obando <anngelao25@gmail.com>
2023-03-26 03:35:12 -0500

    WIP: Add edit and update actions to ChannelsController

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -4,7 +4,36 @@ class ChannelsController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if user.is_admin? && params[:suspended] == true
+      @channel.suspended_at = Time.zone.now
+    end
+    @channel.update_attributes(channel_params)
+    if @channel.save
+      flash["notice"] = "#{@channel.name} has been updated successfully."
+      redirect_to channel_path(@channel)
+    else
+      render :edit
+    end
+  end
+
+  def suspend
+    if user.is_admin?
+      @channel.update(suspended_at: Time.now)
+      redirect_to channel_path(@channel)
+    else
+      redirect_to channel_path(@channel)
+    end
+  end
+
   private
+
+  def channel_param
+    params.require(:channel).permit(:name, :description)
+  end
 
   def set_channel
     @channel = Channel.find(params[:id])

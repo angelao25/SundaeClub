64d57e364d14dfc95722cf813ede8cb66c65a55c
Angel Aguirre Obando <anngelao25@gmail.com>
2023-03-26 03:32:12 -0500

    WIP: Add FactoryBot factories and initial config

---
+++ spec/factories/organisations.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :organisation do
+    id { 1 }
+    name { "MyText" }
+    slug { "MyText" }
+    created_at { "2023-03-24 18:14:16" }
+    updated_at { "2023-03-24 18:14:16" }
+  end
+end

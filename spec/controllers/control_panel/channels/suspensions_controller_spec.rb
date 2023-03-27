1bb3cfc985e64a3446cdfef5dc71c1bd48c1568d
Angel Aguirre Obando <anngelao25@gmail.com>
2023-03-27 18:46:40 -0500

    WIP: Install devise views
    These are all the defaults that are generated when running rails g devise:views

---
+++ spec/controllers/control_panel/channels/suspensions_controller_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+#
+require "rails_helper"
+
+describe ControlPanel::Channels::SuspensionsController, type: :controller do
+  let(:channel) { FactoryBot.create(:channel) }
+
+  context "#create" do
+    #    before { allow(Channel).to receive(:find).with(123).and_return(channel) }
+    it "should suspend the channel" do
+      post :create, params: { channel_id: channel.id }
+      expect(channel.reload.suspended_at).to_not be_nil
+    end
+  end
+end

68172638547bade4b00bdf306702542e3f62b7ce
Angel Aguirre Obando <anngelao25@gmail.com>
2023-03-28 19:19:17 -0500

    WP: Add Site model and migration

---
+++ db/migrate/20230329001304_create_sites.rb
@@ -0,0 +1,12 @@
+class CreateSites < ActiveRecord::Migration[7.0]
+  def change
+    create_table :sites do |t|
+      t.text :name
+      t.text :description
+      t.text :slug
+      t.references :organisation, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

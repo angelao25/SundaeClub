feeb66c5cc55466ea70da990a5bcc893887cae75
Angel Aguirre Obando <anngelao25@gmail.com>
2023-03-30 22:49:08 -0500

    WIP: Add tests for SitesSetup::UsersController

---
+++ spec/controllers/sites_setup/users_controller_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe SitesSetup::UsersController, type: :controller do
+  describe "#new" do
+    context "with a valid site" do
+      let(:site) { FactoryBot.create(:site) }
+
+      it "should return a success response" do
+        get :new, params: { site_id: site.id }
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe "#create" do
+    context "with a valid site" do
+      let(:site) { FactoryBot.create(:site) }
+
+      context "with valid params" do
+        let(:valid_params) do
+          { email: "example@example.com", password: "password" }
+        end
+        let(:create_action) do
+          post :create, params: { site_id: site.id, user: valid_params }
+        end
+
+        it "should create a new user associated with the sites organisation" do
+          expect { create_action }.to change { User.count }.by(1)
+        end
+      end
+    end
+  end
+end
